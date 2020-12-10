87de1a79dd4ae7ea2566e3c5e38f5f3dea1ddf07
Caio Rocha <caiorocha@MacBook-Pro-de-Caio.local>
2020-12-10 16:26:10 -0500

    Step 3: Define currency conversion rates. Define which currency conversions are supported, as well as the rates between each currency.

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -43,7 +43,14 @@ if (amount === undefined || initialCurrency === undefined || targetCurrency === 
 
 // The conversion rates do not have to be accurate, athough this resource contains
 // up-to-date rate information: https://www.xe.com/
-
+var rates = {
+    USD: {
+        CAD: 2,
+    },
+    CAD: {
+        USD: 0.5,
+    },
+};
 
 
 // --------------------------------------------------
